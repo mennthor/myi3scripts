a7eacc4dd58d401d265fe9a08d4bf8e2c472ed11
Thorben Menne <mennthor@aol.com>
2018-04-10 10:51:05 +0200

    Added serialize ndarrays

--- myi3scripts/misc/misc.py
+++ myi3scripts/misc/misc.py
@@ -2,6 +2,8 @@
 
 from __future__ import absolute_import
 
+import numpy as np
+
 
 def arr2str(arr, sep=", ", fmt="{}"):
     """
@@ -9,3 +11,42 @@ def arr2str(arr, sep=", ", fmt="{}"):
     with ``fmt``.
     """
     return sep.join([fmt.format(v) for v in arr])
+
+
+def serialize_ndarrays(d):
+    """
+    Recursively traverse through iterable object ``d`` and convert all occuring
+    ndarrays to lists to make it JSON serializable.
+
+    Note: Works for 1D dicts with ndarrays at first level. Certainly not tested
+          and meant to work for all use cases.
+          Made with code from: http://code.activestate.com/recipes/577504/
+
+    Parameters
+    ----------
+    d : iterable
+        Can be dict, list, set, tuple or frozenset.
+
+    Returns
+    -------
+    d : iterable
+        Same as input, but all ndarrays replaced by lists.
+    """
+    def dict_handler(d):
+        return d.items()
+
+    handlers = {list: enumerate, tuple: enumerate,
+                set: enumerate, frozenset: enumerate,
+                dict: dict_handler}
+
+    def serialize(o):
+        for typ, handler in handlers.items():
+            if isinstance(o, typ):
+                for key, val in handler(o):
+                    if isinstance(val, np.ndarray):
+                        o[key] = val.tolist()
+                    else:
+                        o[key] = serialize_ndarrays(o[key])
+        return o
+
+    return serialize(d)
